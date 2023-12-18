4b5c6c1a55b8bcfb12e261f179c48e65855c7963
csiformk <formk@kermed.fr>
2023-12-18 14:42:30 +0100

    Ajout listes des categories dans une note

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Note;
+use App\Repository\NoteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(NoteRepository $noterepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'notes' => $noterepository->findAll(),
+        ]);
+    }
+    
+    #[Route('/home/{id}', name: 'app_note_show', methods: ['GET'])]
+    public function show(Note $note): Response
+    {
+        return $this->render('home/show.html.twig', [
+            'note' => $note,
         ]);
     }
 }
